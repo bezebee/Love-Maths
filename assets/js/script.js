ff8d8c29ab951d5d59383fb4d1d4619e571f7a4c
Kasia Bee <kas.bogucka@gmail.com>
2021-05-02 10:40:14 +0000

    add code to calculate two random numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,17 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 })
 
+/**
+ * The main game loop, called when the scrupt is first loaded
+ * and after the users answer has been processed
+ */
 function runGame(){
 
+    //Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() *25)+1;
+    let num2 = Math.floor(Math.random() *25)+1;
 }
+
 
 function checkAnswer(){
 
